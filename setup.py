bcd04e3d093e844df1a4a8d17cd4212c8bc33d15
Soren Hansen <soren@canonical.com>
2008-09-04 09:46:01 +0200

    Tagged 0.8.1 (somewhat retroactively)

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 import VMBuilder.plugins
 
 setup(name='VMBuilder',
-      version='0.1',
+      version='0.8.1',
       description='Uncomplicated VM Builder',
       author='Soren Hansen',
       author_email='soren@canonical.com',
-      url='http://launchpad.net/ubuntu-jeos/',
+      url='http://launchpad.net/vmbuilder/',
       packages=['VMBuilder', 'VMBuilder.plugins'] + VMBuilder.plugins.find_plugins(),
       scripts=['vmbuilder'], 
       )
