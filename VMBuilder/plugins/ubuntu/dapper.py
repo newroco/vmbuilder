6f6986023bfb6c537d5882b6a84d0453e2261b47
Nick Barcet <nick.barcet@canonical.com>
2008-10-31 21:00:39 +0100

    a bit more complex than I thought to use the appropriate source during install

--- VMBuilder/plugins/ubuntu/dapper.py
+++ VMBuilder/plugins/ubuntu/dapper.py
@@ -34,6 +34,7 @@ class Dapper(suite.Suite):
                        'amd64' : ['amd64-generic', 'amd64-k8', 'amd64-k8-smp', 'amd64-server', 'amd64-xeon']}
     default_flavour = { 'i386' : 'server', 'amd64' : 'amd64-server' }
     disk_prefix = 'hd'
+    mirror = ''
 
     def check_kernel_flavour(self, arch, flavour):
         return flavour in self.valid_flavours[arch]
@@ -84,6 +85,9 @@ class Dapper(suite.Suite):
 
         logging.debug("Installing ssh keys")
         self.install_authorized_keys()
+
+        logging.debug("Setting up final sources.list")
+        self.install_sources_list("final")
 
         logging.debug("Making sure system is up-to-date")
         self.update()
@@ -168,9 +172,17 @@ class Dapper(suite.Suite):
         run_cmd('sed', '-ie', 's/^# groot.*/# groot %s/g' % bootdev.get_grub_id(), '%s/boot/grub/menu.lst' % self.destdir)
         run_cmd('sed', '-ie', '/^# kopt_2_6/ d', '%s/boot/grub/menu.lst' % self.destdir)
 
-    def install_sources_list(self):
-        self.install_from_template('/etc/apt/sources.list', 'sources.list')
-        self.run_in_target('apt-get', 'update')
+    def install_sources_list(self, final=False):
+        if final:
+            # avoid running a second time if mirror does not change
+            if self.mirror != self.vm.mirror:
+                mirror = self.vm.mirror
+        else:
+            mirror = self.mirror
+        
+        if mirror:
+            self.install_from_template('/etc/apt/sources.list', 'sources.list', { 'mirror' : mirror, 'rmirror' : self.vm.mirror })
+            self.run_in_target('apt-get', 'update')
 
     def install_fstab(self):
         if self.vm.hypervisor.preferred_storage == VMBuilder.hypervisor.STORAGE_FS_IMAGE:
@@ -187,18 +199,21 @@ class Dapper(suite.Suite):
         if self.vm.iso:
             isodir = '%s/isomnt' % self.destdir
             run_cmd ('mkdir', isodir)
-            run_cmd ('mount', '-o loop', '-t iso9660', self.vm.iso, isodir)
-            cmd += ['file:///%s' % isodir]
+            self.vm.add_clean_cmd('rmdir', isodir)
+            run_cmd ('mount', '-o', 'loop', '-t', 'iso9660', self.vm.iso, isodir)
+            self.vm.add_clean_cmd('umount', isodir)
+            self.mirror = 'file://%s' % isodir
         elif self.vm.install_mirror:
-            cmd += [self.vm.install_mirror]
-        elif self.vm.mirror:
-            cmd += [self.vm.mirror]
+            self.mirror = self.vm.install_mirror
+        else:
+            self.mirror = self.vm.mirror
+
+        cmd += [self.mirror]
 
         run_cmd(*cmd)
 
         if self.vm.iso:
-            run_cmd ('umount', isodir)
-            run_cmd ('rmdir', isodir)
+            self.mirror = "file:///isomnt"
 
     def install_kernel(self):
         self.install_from_template('/etc/kernel-img.conf', 'kernelimg', { 'updategrub' : self.updategrub }) 
