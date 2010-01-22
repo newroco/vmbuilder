a1d59071f44c6c6420b606e6eb3070242ed86794
Soren Hansen <soren@ubuntu.com>
2010-01-22 15:53:28 +0100

    Add a Setting and SettingGroup concept to the Plugin class.
    This is expected to replace the custom OptionParser thing we've been
    using so far.
    
    Of particular interest is the fact that the VM object itself does not
    get these attributes set. Instead, the VM object gets a ._config
    attribute which is a dict that maps option names to Setting objects. The
    Setting objects then holds the values. This will require changes across
    all of VMBuilder.
    
    Tests are included.

--- VMBuilder/plugins/__init__.py
+++ VMBuilder/plugins/__init__.py
@@ -17,8 +17,11 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 import os
+import re
+
 import VMBuilder
 from VMBuilder.util import run_cmd
+from VMBuilder.exception import VMBuilderException
 
 def load_plugins():
     for plugin in find_plugins():
@@ -36,6 +39,7 @@ def find_plugins():
 class Plugin(object):
     def __init__(self, vm):
         self.vm = vm
+        self._setting_groups = []
         self.register_options()
 
     def register_options(self):
@@ -75,3 +79,141 @@ class Plugin(object):
         if not self.vm.fsmounted:
             raise VMBuilderException('install_from_template called while file system is not mounted')
         return run_cmd('chroot', self.vm.installdir, *args, **kwargs)
+
+    # Settings
+    class SettingGroup(object):
+        def __init__(self, plugin, vm, name):
+            # The plugin that owns this setting
+            self.plugin = plugin
+            # The VM object
+            self.vm = vm
+            # Name of the Setting Group
+            self.name = name
+            # A list of Setting objects
+            self._settings = []
+
+        def add_setting(self, *args, **kwargs):
+            # kwarg['type'] is used to determine which type of Setting object to create
+            # but we don't want to pass it on to its __init__.
+            if 'type' in kwargs:
+                type = kwargs['type']
+                del kwargs['type']
+            else:
+                type = 'str'
+
+            if type == 'str':
+                setting = self.plugin.StringSetting(self, *args, **kwargs)
+            elif type == 'bool':
+                setting = self.plugin.BooleanSetting(self, *args, **kwargs)
+            elif type == 'list':
+                setting = self.plugin.ListSetting(self, *args, **kwargs)
+            elif type == 'int':
+                setting = self.plugin.IntSetting(self, *args, **kwargs)
+            else:
+                raise VMBuilderException("Unknown setting type: '%s' (Plugin: '%s', Setting group: '%s', Setting: '%s')" % 
+                                            (type,
+                                             self.plugin.__module__,
+                                             self.name,
+                                             args[0]))
+            self._settings.append(setting)
+
+    class Setting(object):
+        default = None
+
+        def __init__(self, setting_group, name, metavar=None, help=None, extra_args=None, action=None, **kwargs):
+            # The Setting Group object that owns this Setting
+            self.setting_group = setting_group
+            # The name if the setting
+            name_regex = '[a-z0-9-]+$'
+            if not re.match(name_regex, name):
+                raise VMBuilderException('Invalid name for Setting: %s. Must match regex: %s' % (name, name_regex))
+            else:
+                self.name = name
+
+            self.default = kwargs.get('default', self.default)
+            self.help = help
+            # Alternate names (for the CLI)
+            self.extra_args = extra_args or []
+            self.value = None
+            self.value_set = False
+
+            if self.name in self.setting_group.vm._config:
+                raise VMBuilderException("Setting named %s already exists. Previous definition in %s/%s/%s." % 
+                                            (self.name,
+                                             self.setting_group.plugin.__name__,
+                                             self.setting_group.plugin._config[self.name].setting_group.name,
+                                             self.setting_group.plugin._config[self.name].name))
+
+            self.setting_group.vm._config[self.name] = self
+
+        def get_value(self):
+            """
+            If a value has previously been set, return it.
+            If not, return the default value.
+            """
+
+            if self.value_set:
+                return self.value
+            else:
+                return self.default
+
+        def set_default(self, value):
+            """
+            Set a new default value.
+            """
+            self.check_value(value)
+            self.default = value
+
+        def set_value(self, value):
+            """
+            Set a new value.
+            """
+            self.check_value(value)
+            self.value = value
+            self.value_set = True
+
+    class ListSetting(Setting):
+        def __init__(self, *args, **kwargs):
+            self.default = []
+            super(Plugin.ListSetting, self).__init__(*args, **kwargs)
+
+        def check_value(self, value):
+            if not type(value) == list:
+                raise VMBuilderException('%r is type %s, expected list.' % (value, type(value)))
+
+    class IntSetting(Setting):
+        def check_value(self, value):
+            if not type(value) == int:
+                raise VMBuilderException('%r is type %s, expected int.' % (value, type(value)))
+
+    class BooleanSetting(Setting):
+        def check_value(self, value):
+            if not type(value) == bool:
+                raise VMBuilderException('%r is type %s, expected bool.' % (value, type(value)))
+
+    class StringSetting(Setting):
+        def check_value(self, value):
+            if not type(value) == str:
+                raise VMBuilderException('%r is type %s, expected str.' % (value, type(value)))
+
+    def setting_group(self, name):
+        setting_group = self.SettingGroup(self, self.vm, name)
+        self._setting_groups.append(setting_group)
+        return setting_group
+
+    def get_setting(self, name):
+        if not name in self._config:
+            raise VMBuilderException('Unknown config key: %s' % name)
+        return self._config[name].get_value()
+
+    def set_setting_default(self, name, value):
+        if not name in self.vm._config:
+            raise VMBuilderException('Unknown config key: %s' % name)
+        self._config[name].set_default(value)
+
+    def set_setting(self, name, value):
+        if not name in self.vm._config:
+            raise VMBuilderException('Unknown config key: %s' % name)
+        self._config[name].set_value(value)
+
+
---
+++ test/plugin_tests.py
@@ -0,0 +1,88 @@
+import unittest
+
+import VMBuilder.plugins
+from   VMBuilder.exception import VMBuilderException
+
+class TestPluginsSettings(unittest.TestCase):
+    class VM(VMBuilder.plugins.Plugin):
+        def __init__(self, *args, **kwargs):
+            self._config = {}
+            self.vm = self
+
+    class TestPlugin(VMBuilder.plugins.Plugin):
+        pass
+
+    def setUp(self):
+        self.vm = self.VM()
+        self.plugin = self.TestPlugin(self.vm)
+
+    def test_add_setting_group_and_setting(self):
+        setting_group = self.plugin.setting_group('Test Setting Group')
+        self.assertTrue(setting_group in self.plugin._setting_groups, "Setting not added correctly to plugin's registry of setting groups.")
+
+        setting_group.add_setting('testsetting')
+        self.assertEqual(self.vm.get_setting('testsetting'), None, "Setting's default value is not None.")
+
+        self.vm.set_setting_default('testsetting', 'newdefault')
+        self.assertEqual(self.vm.get_setting('testsetting'), 'newdefault', "Setting does not return custom default value when no value is set.")
+
+        self.vm.set_setting('testsetting', 'foo')
+        self.assertEqual(self.vm.get_setting('testsetting'), 'foo', "Setting does not return set value.")
+
+        self.vm.set_setting_default('testsetting', 'newerdefault')
+        self.assertEqual(self.vm.get_setting('testsetting'), 'foo', "Setting does not return set value after setting new default value.")
+
+    def test_invalid_type_raises_exception(self):
+        setting_group = self.plugin.setting_group('Test Setting Group')
+        self.assertRaises(VMBuilderException, setting_group.add_setting, 'oddsetting', type='odd')
+
+    def test_invalid_type_setting_raises_exception(self):
+        setting_group = self.plugin.setting_group('Test Setting Group')
+
+        setting_group.add_setting('strsetting')
+        self.vm.set_setting('strsetting', '')
+        self.vm.set_setting_default('strsetting', '')
+        self.assertRaises(VMBuilderException, self.vm.set_setting, 'strsetting', 0)
+        self.assertRaises(VMBuilderException, self.vm.set_setting_default, 'strsetting', 0)
+        self.assertRaises(VMBuilderException, self.vm.set_setting, 'strsetting', True)
+        self.assertRaises(VMBuilderException, self.vm.set_setting_default, 'strsetting', True)
+        self.assertRaises(VMBuilderException, self.vm.set_setting, 'strsetting', [])
+        self.assertRaises(VMBuilderException, self.vm.set_setting_default, 'strsetting', [])
+
+        setting_group.add_setting('intsetting', type='int')
+        self.assertRaises(VMBuilderException, self.vm.set_setting, 'intsetting', '')
+        self.assertRaises(VMBuilderException, self.vm.set_setting_default, 'intsetting', '')
+        self.vm.set_setting('intsetting', 0)
+        self.vm.set_setting_default('intsetting', 0)
+        self.assertRaises(VMBuilderException, self.vm.set_setting, 'intsetting', True)
+        self.assertRaises(VMBuilderException, self.vm.set_setting_default, 'intsetting', True)
+        self.assertRaises(VMBuilderException, self.vm.set_setting, 'intsetting', [])
+        self.assertRaises(VMBuilderException, self.vm.set_setting_default, 'intsetting', [])
+
+        setting_group.add_setting('boolsetting', type='bool')
+        self.assertRaises(VMBuilderException, self.vm.set_setting, 'boolsetting', '')
+        self.assertRaises(VMBuilderException, self.vm.set_setting_default, 'boolsetting', '')
+        self.assertRaises(VMBuilderException, self.vm.set_setting, 'boolsetting', 0)
+        self.assertRaises(VMBuilderException, self.vm.set_setting_default, 'boolsetting', 0)
+        self.vm.set_setting('boolsetting', True)
+        self.vm.set_setting_default('boolsetting', True)
+        self.assertRaises(VMBuilderException, self.vm.set_setting, 'boolsetting', [])
+        self.assertRaises(VMBuilderException, self.vm.set_setting_default, 'boolsetting', [])
+
+        setting_group.add_setting('listsetting', type='list')
+        self.assertRaises(VMBuilderException, self.vm.set_setting, 'listsetting', '')
+        self.assertRaises(VMBuilderException, self.vm.set_setting_default, 'listsetting', '')
+        self.assertRaises(VMBuilderException, self.vm.set_setting, 'listsetting', 0)
+        self.assertRaises(VMBuilderException, self.vm.set_setting_default, 'listsetting', 0)
+        self.assertRaises(VMBuilderException, self.vm.set_setting, 'listsetting', True)
+        self.assertRaises(VMBuilderException, self.vm.set_setting_default, 'listsetting', True)
+        self.vm.set_setting('listsetting', [])
+        self.vm.set_setting_default('listsetting', [])
+
+    def test_set_setting_raises_exception_on_invalid_setting(self):
+        self.assertRaises(VMBuilderException, self.vm.set_setting_default, 'testsetting', 'newdefault')
+
+    def test_add_setting(self):
+        setting_group = self.plugin.setting_group('Test Setting Group')
+
+
