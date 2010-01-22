86d248ed160381b686206cec0543a8fe4cc1db40
Soren Hansen <soren@ubuntu.com>
2010-01-22 14:27:16 +0100

    Add "priority" attribute to plugins.
    Upon registration, plugins will be internally ordered according to this priority.
    This is used to make call_hooks call things in the right order.
    
    Includes test case.

--- test/util_tests.py
+++ test/util_tests.py
@@ -7,3 +7,25 @@ class TestUtils(unittest.TestCase):
     def test_run_cmd(self):
         self.assertTrue("foobarbaztest" in run_cmd("env", env={'foobarbaztest' : 'bar' }))
 
+    def test_plugin_priority(self):
+        class Plugin(object):
+            priority = 10
+
+        class PluginA(Plugin):
+            pass
+
+        class PluginB(Plugin):
+            priority = 5
+
+        class PluginC(Plugin):
+            priority = 15
+
+        saved_plugins = VMBuilder._plugins
+        VMBuilder._plugins = []
+        VMBuilder.register_plugin(PluginA)
+        VMBuilder.register_plugin(PluginB)
+        VMBuilder.register_plugin(PluginC)
+        self.assertEqual(VMBuilder._plugins[0], PluginB)
+        self.assertEqual(VMBuilder._plugins[1], PluginA)
+        self.assertEqual(VMBuilder._plugins[2], PluginC)
+        VMBuilder._plugins = saved_plugins
