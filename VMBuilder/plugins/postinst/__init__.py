f6c45f369555dee182efb6f0a4a08fb681195a20
soren@canonical.com <>
2010-02-24 09:25:28 +0100

    Re-enable the postinst plugin. If trying to install a file to a nonexisting directory, create the directory first.

--- VMBuilder/plugins/postinst/__init__.py
+++ VMBuilder/plugins/postinst/__init__.py
@@ -32,43 +32,49 @@ class postinst(Plugin):
     name ='Post install plugin'
 
     def register_options(self):
-        group = self.context.setting_group('Post install actions')
-        group.add_option('--copy', metavar='FILE', help="Read 'source dest' lines from FILE, copying source files from host to dest in the guest's file system.")
-        group.add_option('--execscript', '--exec', metavar='SCRIPT', help="Run SCRIPT after distro installation finishes. Script will be called with the guest's chroot as first argument, so you can use 'chroot $1 <cmd>' to run code in the virtual machine.")
-        self.context.register_setting_group(group)
+        group = self.setting_group('Post install actions')
+        group.add_setting('copy', metavar='FILE', help="Read 'source dest' lines from FILE, copying source files from host to dest in the guest's file system.")
+        group.add_setting('execscript', extra_args=['--exec'], metavar='SCRIPT', help="Run SCRIPT after distro installation finishes. Script will be called with the guest's chroot as first argument, so you can use 'chroot $1 <cmd>' to run code in the virtual machine.")
 
     def preflight_check(self):
-        if self.context.copy:
-            logging.debug("Checking if --copy PATH exists: %s" % self.context.copy)
-            if not(os.path.isfile(self.context.copy)):
-                raise VMBuilderUserError('The path to the --copy directives is invalid: %s. Make sure you are providing a full path.' % self.context.copy)
+        copy = self.context.get_setting('copy')
+        if copy:
+            logging.debug("Checking if copy PATH exists: %s" % copy)
+            if not(os.path.isfile(copy)):
+                raise VMBuilderUserError('The path to the copy directive is invalid: %s. Make sure you are providing a full path.' % copy)
                 
-        if self.context.execscript:
-            logging.debug("Checking if --exec PATH exists: %s" % self.context.execscript)
-            if not(os.path.isfile(self.context.execscript)):
-                raise VMBuilderUserError('The path to the --execscript file is invalid: %s. Make sure you are providing a full path.' % self.context.execscript) 
+        execscript = self.context.get_setting('execscript')
+        if execscript:
+            logging.debug("Checking if exec PATH exists: %s" % execscript)
+            if not(os.path.isfile(execscript)):
+                raise VMBuilderUserError('The path to the execscript file is invalid: %s. Make sure you are providing a full path.' % execscript) 
 
-            logging.debug("Checking permissions of --exec PATH: %s" % self.context.execscript)
-            if not os.access(self.context.execscript, os.X_OK|os.R_OK):
-                raise VMBuilderUserError('The path to the --execscript file has invalid permissions: %s. Make sure the path is readable and executable.' % self.context.execscript)
+            logging.debug("Checking permissions of exec PATH: %s" % execscript)
+            if not os.access(execscript, os.X_OK|os.R_OK):
+                raise VMBuilderUserError('The path to the execscript file has invalid permissions: %s. Make sure the path is readable and executable.' % execscript)
 
     def post_install(self):
-        if self.context.copy:
-            logging.info("Copying files specified by --copy in: %s" % self.context.copy)
+        copy = self.context.get_setting('copy')
+        execscript = self.context.get_setting('execscript')
+        if copy:
+            logging.info("Copying files specified by copy in: %s" % copy)
             try:
-                for line in file(self.context.copy):
+                for line in file(copy):
                     pair = line.strip().split(' ')
                     if len(pair) < 2: # skip blank and incomplete lines
                         continue
-                    util.run_cmd('cp', '-LpR', pair[0], '%s%s' % (self.context.installdir, pair[1]))
+                    directory = '%s%s' % (self.context.chroot_dir, os.path.dirname(pair[1]))
+                    if not os.path.exists(directory):
+                        os.makedirs(directory)
+                    util.run_cmd('cp', '-LpR', pair[0], '%s%s' % (self.context.chroot_dir, pair[1]))
 
             except IOError, (errno, strerror):
-                raise VMBuilderUserError("%s executing --copy directives: %s" % (errno, strerror))
+                raise VMBuilderUserError("%s executing copy directives: %s" % (errno, strerror))
 
-        if self.context.execscript:
-            logging.info("Executing script: %s" % self.context.execscript)
-            util.run_cmd(self.context.execscript, self.vm.installdir)
+        if execscript:
+            logging.info("Executing script: %s" % execscript)
+            util.run_cmd(execscript, self.context.chroot_dir)
 
         return True
 
-#register_plugin(postinst)
+register_distro_plugin(postinst)
