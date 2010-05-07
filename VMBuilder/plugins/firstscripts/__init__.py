c9f6d40fc5d2206a605f8fba712716b3dfeaf688
Soren Hansen <soren@linux2go.dk>
2010-05-07 12:50:57 +0200

    Revive firstscripts plugins. This fixes the --firstboot and --firstlogin options, both of which went missing with 0.12.

--- VMBuilder/plugins/firstscripts/__init__.py
+++ VMBuilder/plugins/firstscripts/__init__.py
@@ -1,7 +1,7 @@
 #
 #    Uncomplicated VM Builder
-#    Copyright (C) 2007-2009 Canonical Ltd.
-#    
+#    Copyright (C) 2007-2010 Canonical Ltd.
+#
 #    See AUTHORS for list of contributors
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -28,36 +28,38 @@ class Firstscripts(Plugin):
     name = 'First-Scripts plugin'
 
     def register_options(self):
-        group = self.context.setting_group('Scripts')
-        group.add_option('--firstboot', metavar='PATH', default='', help='Specify a script that will be copied into the guest and executed the first time the machine boots.  This script must not be interactive.')
-        group.add_option('--firstlogin', metavar='PATH', default='', help='Specify a script that will be copied into the guest and will be executed the first time the user logs in. This script can be interactive.')
-        self.context.register_setting_group(group)
+        group = self.setting_group('Scripts')
+        group.add_setting('firstboot', metavar='PATH', help='Specify a script that will be copied into the guest and executed the first time the machine boots.  This script must not be interactive.')
+        group.add_setting('firstlogin', metavar='PATH', help='Specify a script that will be copied into the guest and will be executed the first time the user logs in. This script can be interactive.')
 
     def preflight_check(self):
-        
-        if self.context.firstboot:
-            logging.debug("Checking if firstboot script %s exists" % (self.context.firstboot,))
-            if not(os.path.isfile(self.context.firstboot)):
-                raise VMBuilderUserError('The path to the first-boot script is invalid: %s. Make sure you are providing a full path.' % self.context.firstboot)
-                
-        if self.context.firstlogin:
-            logging.debug("Checking if first login script %s exists" % (self.context.firstlogin,))
-            if not(os.path.isfile(self.context.firstlogin)):
-                raise VMBuilderUserError('The path to the first-login script is invalid: %s.  Make sure you are providing a full path.' % self.context.firstlogin)
+        firstboot = self.context.get_setting('firstboot')
+        if firstboot:
+            logging.debug("Checking if firstboot script %s exists" % (firstboot,))
+            if not(os.path.isfile(firstboot) and firstboot.startswith('/')):
+                raise VMBuilderUserError('The path to the first-boot script is invalid: %s. Make sure you are providing a full path.' % firstboot)
+
+        firstlogin = self.context.get_setting('firstlogin')
+        if firstlogin:
+            logging.debug("Checking if first login script %s exists" % (firstlogin,))
+            if not(os.path.isfile(firstlogin) and firstlogin.startswith('/')):
+                raise VMBuilderUserError('The path to the first-login script is invalid: %s.  Make sure you are providing a full path.' % firstlogin)
 
     def post_install(self):
-        logging.debug("Installing firstboot script %s" % (self.context.firstboot,))
-        if self.context.firstboot:
-            self.context.install_file('/root/firstboot.sh', source=self.vm.firstboot, mode=0700)
-            os.rename('%s/etc/rc.local' % self.context.installdir, '%s/etc/rc.local.orig' % self.vm.installdir)
+        firstboot = self.context.get_setting('firstboot')
+        if firstboot:
+            logging.debug("Installing firstboot script %s" % (firstboot,))
+            self.context.install_file('/root/firstboot.sh', source=firstboot, mode=0700)
+            os.rename('%s/etc/rc.local' % self.context.chroot_dir, '%s/etc/rc.local.orig' % self.context.chroot_dir)
             self.install_from_template('/etc/rc.local', 'firstbootrc', mode=0755)
 
-        logging.debug("Installing first login script %s" % (self.context.firstlogin,))
-        if self.context.firstlogin:
-            self.context.install_file('/root/firstlogin.sh', source=self.vm.firstlogin, mode=0755)
-            os.rename('%s/etc/bash.bashrc' % self.context.installdir, '%s/etc/bash.bashrc.orig' % self.vm.installdir)
+        firstlogin = self.context.get_setting('firstlogin')
+        if firstlogin:
+            logging.debug("Installing first login script %s" % (firstlogin,))
+            self.context.install_file('/root/firstlogin.sh', source=firstlogin, mode=0755)
+            os.rename('%s/etc/bash.bashrc' % self.context.chroot_dir, '%s/etc/bash.bashrc.orig' % self.context.chroot_dir)
             self.install_from_template('/etc/bash.bashrc', 'firstloginrc')
 
         return True
 
-#register_plugin(Firstscripts)
+register_distro_plugin(Firstscripts)
