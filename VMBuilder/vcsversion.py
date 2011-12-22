5c74e73c570a38fbb473131ae9d98fb73bc126f9
Serge Hallyn <serge.hallyn@canonical.com>
2011-12-22 10:49:31 -0600

    Fix tab/spaces mixup

--- VMBuilder/vcsversion.py
+++ VMBuilder/vcsversion.py
@@ -5,11 +5,11 @@ So don't edit it. :)
 """
 
 version_info = {'branch_nick': u'vmbuilder',
- 'build_date': '2011-12-22 10:10:38 -0600',
+ 'build_date': '2011-12-22 10:49:13 -0600',
  'clean': None,
- 'date': '2011-12-22 09:13:31 -0600',
- 'revision_id': 'serge.hallyn@canonical.com-20111222151331-ua1zpwvuycscjpcy',
- 'revno': '473'}
+ 'date': '2011-12-22 10:11:20 -0600',
+ 'revision_id': 'serge.hallyn@canonical.com-20111222161120-73vtdj1lh029ax92',
+ 'revno': '475'}
 
 revisions = {}
 
