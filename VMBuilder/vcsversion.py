f996acb06a80a3c93183442853dc1da86f1f5d09
Serge Hallyn <serge.hallyn@ubuntu.com>
2015-10-29 20:44:21 -0500

    update version info...
    maybe unneeded magic.

--- VMBuilder/vcsversion.py
+++ VMBuilder/vcsversion.py
@@ -5,11 +5,11 @@ So don't edit it. :)
 """
 
 version_info = {'branch_nick': u'vmbuilder',
- 'build_date': '2011-12-22 10:49:13 -0600',
+ 'build_date': '2015-10-29 20:43:19 -0600',
  'clean': None,
- 'date': '2011-12-22 10:11:20 -0600',
+ 'date': '2015-10-29 20:43:19 -0600',
  'revision_id': 'serge.hallyn@canonical.com-20111222161120-73vtdj1lh029ax92',
- 'revno': '475'}
+ 'revno': '494'}
 
 revisions = {}
 
